55528a304073d943325b5c0194aab28468cddaef
Douglas Henrique <dev.dhbarboza@gmail.com>
2021-06-19 23:44:27 -0300

    Update

--- Application/Src/__init__.py
+++ Application/Src/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/recipe'
+app.config.from_object('config')
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from Src.controllers import engine
